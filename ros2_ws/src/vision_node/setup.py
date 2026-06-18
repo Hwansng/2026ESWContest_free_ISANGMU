11a5c4b17efdc99e437792eca7103460c5fcee3b
진우 <jinu@hazardbot.local>
2026-06-18 17:27:28 +0900

    feat: vision_node 초기 구현 (HSV + minAreaRect)
    cv2.VideoCapture(0) 직접 오픈 방식.
    색상 마스크 -> 컨투어 -> minAreaRect로 중심/크기/방위각 추출.
    종횡비 기반 형태 판정 포함.

--- ros2_ws/src/vision_node/setup.py
+++ ros2_ws/src/vision_node/setup.py
@@ -1,36 +1,20 @@
-"""vision_node ament_python 패키지 설치 정의.
-
-console_scripts로 3개 실행 노드(vision_node, mock_amr_publisher,
-dummy_subscriber)를 등록하고, config의 HSV 설정 파일을 share에 설치한다.
-"""
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'vision_node'
 
 setup(
     name=package_name,
-    version='0.1.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        # ament 패키지 인덱스 마커
-        ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        # HSV 캘리브레이션 파일을 share/vision_node/config에 설치
-        ('share/' + package_name + '/config', ['config/hsv_calibration.yaml']),
-    ],
+    version='0.0.1',
+    packages=[package_name],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='seunghwan',
-    maintainer_email='snghwan68@gmail.com',
-    description='HazardBot 비전 노드 (HSV 색상 분류 + minAreaRect 방위각)',
+    maintainer='pi',
+    maintainer_email='pi@todo.todo',
+    description='Vision Node OpenCV HSV',
     license='MIT',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'vision_node = vision_node.vision_node:main',
-            'mock_amr_publisher = vision_node.mock_amr_publisher:main',
-            'dummy_subscriber = vision_node.dummy_subscriber:main',
         ],
     },
 )
